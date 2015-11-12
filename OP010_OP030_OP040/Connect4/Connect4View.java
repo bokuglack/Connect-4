65d2abf0b7a9823a0d6e638f0c393c5d7f833068
GEK0256 <harona00@gmail.com>
2015-11-12 11:49:05 -0500

    Update Connect4View.java

--- OP010_OP030_OP040/Connect4/Connect4View.java
+++ OP010_OP030_OP040/Connect4/Connect4View.java
@@ -1,1 +1,16 @@
 
+public class Connect4View implements KeyListener{
+  private Board currentBoard;
+  
+  public Connect4view(Board b){
+    currentBoard = b;
+  }
+  
+  public void setBoard(Board b){
+    currentBoard = b;
+  }
+  
+  public void updateDisplay(){
+    System.out.println(currentBoard.toString());
+  }
+}
