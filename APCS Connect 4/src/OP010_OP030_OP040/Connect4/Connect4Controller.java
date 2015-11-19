f5265ae5f02ac6662aea9a2c309c68e117eb98cc
GEK0256 <harona00@gmail.com>
2015-11-19 11:19:50 -0500

    Update Connect4Controller.java

--- APCS Connect 4/src/OP010_OP030_OP040/Connect4/Connect4Controller.java
+++ APCS Connect 4/src/OP010_OP030_OP040/Connect4/Connect4Controller.java
@@ -4,15 +4,48 @@ import java.awt.event.KeyListener;
 
 public class Connect4Controller implements KeyListener{
   private Board board;
-  private Connect4View view;
-  
-  public Connect4Controller(Board b, Connect4View v){
-    board = b;
-    view = v;
-  }
-  
-  public void runGame(){
-    
-  }
-  
+	private Connect4View view;
+	private boolean cont = false;
+
+	public Connect4Controller(Board b, Connect4View v){
+		board = b;
+		view = v;
+	}
+
+	public void runGame(){
+		if(board.getTurn()){
+			System.out.println("Player 1's turn.\n" + board.toString());
+			while(!cont){	
+			}
+		}
+		else{
+			System.out.println("Player 2's turn.\n" + board.toString());
+			while(!cont){	
+			}
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			board.move(board.turn, 1);
+			System.out.println(board.toString());
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			board.move(board.turn, -1);
+			System.out.println(board.toString());
+		}
+		if(e.getKeyCode() == KeyEvent.VK_SPACE){
+			board.dropChip();
+			cont = true;
+		}
+	}
+
+	public void keyPressed(KeyEvent e) {
+	}
+
+	
+	public void keyReleased(KeyEvent e) {
+	}
 }
